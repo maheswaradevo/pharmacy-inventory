9b7a450d559a211c2191330dbbd4926b99de2484
Maheswara Devo <pundadevo21@gmail.com>
2021-12-01 12:04:57 +0800

    add login menu and program menu

--- main.c
+++ main.c
@@ -1,8 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Person
+{
+    char username[255];
+    char password[255];
+} Person;
+
 int main()
 {
-    printf("Hello\n");
+    int res;
+    struct Person p;
+    printf("Pharmacy Inventory\n");
+
+    //Login menu to user or admin
+    printf("=================\n");
+    printf("      Login      \n");
+    printf("=================\n");
+    printf("Username : ");
+    scanf("%[^\n]s", &p.username);
+    printf("Password : ");
+    scanf("%[^\n]s", &p.password);
+    res = login(p.username, p.password);
+    if (res == 1)
+    {
+        printf("================\n");
+        printf("  Menu Program  \n");
+        printf("================\n");
+        printf("1. Input Barang \n");
+        printf("2. Ambil Barang \n");
+        printf("3. Sort Barang  \n");
+        printf("Masukkan Pilihan : ");
+    }
+    else
+    {
+        //User get inventory display (view only)
+    }
     return 0;
 }
