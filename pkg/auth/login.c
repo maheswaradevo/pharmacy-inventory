3c7c350ff29fec55fc2e66ff154ffea089f196e8
Arthya Andika <74721722+mistykz@users.noreply.github.com>
2021-12-14 20:15:02 +0800

    Update login.c

--- pkg/auth/login.c
+++ pkg/auth/login.c
@@ -10,57 +10,6 @@ int hashValue(char *pass, int size)
     return value & size;
 }
 //START OF ADMIN'S FUNCTION
-int adminMode()
-{
-    system("cls");
-    int menu, pilih;
-    while (1)
-    {
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|       PHARMACY INVENTORY      |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|--------------USER-------------|*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        1. MENU 1              |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        2. MENU 2              |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        3. MENU 3              |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        4. MENU 4              |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        5. MENU 5	          |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        6. EXIT                |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*| Pilih Menu :");
-        scanf("%d", &pilih);
-
-        switch (pilih)
-        {
-        case 1:
-            //menu 1
-            break;
-        case 2:
-            //menu 2
-            break;
-        case 3:
-            //menu 3
-            break;
-        case 4:
-            //menu 4
-            break;
-        case 5:
-            //menu 5
-            break;
-        case 6:
-            exit(0);
-        default:
-            printf("|*| Input tidak ada dalam menu!\n");
-        }
-    }
-    return 0;
-}
 int loginAdminAcc()
 {
     system("cls");
@@ -96,58 +45,6 @@ int loginAdminAcc()
 //END OF ADMIN'S FUNCTION
 
 //START OF USER'S FUNCTION
-int userMode()
-{
-    system("cls");
-    int menu, pilih;
-    while (1)
-    {
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|       PHARMACY INVENTORY      |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|--------------USER-------------|*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        1. MENU 1              |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        2. MENU 2              |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        3. MENU 3              |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        4. MENU 4              |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        5. MENU 5	          |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        6. EXIT                |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*| Pilih Menu :");
-        scanf("%d", &pilih);
-
-        switch (pilih)
-        {
-        case 1:
-            //menu 1
-            break;
-        case 2:
-            //menu 2
-            break;
-        case 3:
-            //menu 3
-            break;
-        case 4:
-            //menu 4
-            break;
-        case 5:
-            //menu 5
-            break;
-        case 6:
-            exit(0);
-        default:
-            printf("|*| Input tidak ada dalam menu!\n");
-        }
-    }
-    return 0;
-}
-
 int createUserAcc()
 {
     system("cls");
@@ -243,3 +140,4 @@ int loginUserAcc()
         return 0;
     }
 }
+//END OF USER'S FUNCTION
