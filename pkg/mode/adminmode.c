95dd641c6a3b584689161a006245bc3ef96d7d96
Arthya Andika <74721722+mistykz@users.noreply.github.com>
2021-12-15 00:20:31 +0800

    Update adminmode.c

--- pkg/mode/adminmode.c
+++ pkg/mode/adminmode.c
@@ -6,24 +6,21 @@ int adminMode()
     int menu, pilih;
     while (1)
     {
+    	system("cls");
         printf("|*|-------------------------------|*|\n");
         printf("|*|       PHARMACY INVENTORY      |*|\n");
         printf("|*|-------------------------------|*|\n");
         printf("|*|-------------ADMIN-------------|*|\n");
         printf("|*|-------------------------------|*|\n");
-        printf("|*|        1. MENU 1              |*|\n");
+        printf("|*|      1. MENAMBAH STOK OBAT    |*|\n");
         printf("|*|-------------------------------|*|\n");
-        printf("|*|        2. MENU 2              |*|\n");
+        printf("|*|      2. MENGAMBIL STOK OBAT   |*|\n");
         printf("|*|-------------------------------|*|\n");
-        printf("|*|        3. MENU 3              |*|\n");
+        printf("|*|      3. LIHAT DATA OBAT       |*|\n");
         printf("|*|-------------------------------|*|\n");
-        printf("|*|        4. MENU 4              |*|\n");
+        printf("|*|      4. EXIT                  |*|\n");
         printf("|*|-------------------------------|*|\n");
-        printf("|*|        5. MENU 5	          |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*|        6. EXIT                |*|\n");
-        printf("|*|-------------------------------|*|\n");
-        printf("|*| Pilih Menu :");
+        printf("|*| Pilih Menu : ");
         scanf("%d", &pilih);
 
         switch (pilih)
@@ -38,16 +35,11 @@ int adminMode()
             //menu 3
             break;
         case 4:
-            //menu 4
-            break;
-        case 5:
-            //menu 5
-            break;
-        case 6:
             exit(0);
         default:
             printf("|*| Input tidak ada dalam menu!\n");
-        }
+			printf("|*| "); system("pause");
+		}
     }
     return 0;
 }
