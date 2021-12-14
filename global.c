c46d32acec2096a42c0a95cd71a41847115c08e8
Arthya Andika <74721722+mistykz@users.noreply.github.com>
2021-12-14 18:15:36 +0800

    create new file "global.c"
    agar project lebih terstruktur

---
+++ global.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//START OF GLOBALS FUNCTION
+struct Person
+{
+    char username[255];
+    char password[255];
+} Person;
+
+int hashValue(char *pass, int size)
+{
+    int value = 0;
+    for (int i = 0; i < sizeof(pass) / sizeof(pass[0]); i++)
+    {
+        value += pass[i];
+    }
+    return value % size;
+}
+
+struct dateExp
+{
+    int date;
+    int month;
+    int year;
+} dateExp;
+
+struct Item
+{
+    char itemName[255];
+    struct dateExp;
+} Item;
+
+int hashValue(char *pass, int size)
+{
+    int value = 0;
+    for (int i = 0; i < sizeof(pass) / sizeof(pass[0]); i++)
+    {
+        value += pass[i];
+    }
+    return value % size;
+}
+//END OF GLOBALS FUNCTION
