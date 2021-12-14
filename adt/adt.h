0c535e9b373b585130ffff6d8480b5c2809f28f6
Arthya Andika <74721722+mistykz@users.noreply.github.com>
2021-12-14 20:38:56 +0800

    Update adt.h

--- adt/adt.h
+++ adt/adt.h
@@ -1,5 +1,33 @@
 #ifndef ADT_DOT_H
 #define ADT_DOT_H
+
 #include <stdio.h>
 #include <stdlib.h>
+
+struct Person
+{
+    char username[255];
+    char password[255];
+};
+
+struct dateExp
+{
+    int date;
+    int month;
+    int year;
+};
+
+struct Item
+{
+    char itemName[255];
+    struct dateExp exp;
+};
+struct Item b[50];
+
+struct stack
+{
+    int top;
+};
+struct stack s;
+
 #endif
