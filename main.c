2e31b80ec44df8475b86753a3122bba0b8fe0427
Arthya Andika <74721722+mistykz@users.noreply.github.com>
2021-12-14 17:00:53 +0800

    update main, user and admin function

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <time.h>
 
+//START OF GLOBALS FUNCTION
 struct Person
 {
     char username[255];
@@ -41,18 +42,23 @@ int hashValue(char *pass, int size)
     }
     return value % size;
 }
+//END OF GLOBALS FUNCTION
 
-//Prototype Function of User
+//PROTOTYPE FUNCTION OF ADMIN
+int adminMode();
+int loginAdminAcc();
+
+//PROTOTYPE FUNCTION OF USER
+int userMode();
 int createUserAcc();
 int loginUserAcc();
 
-//Prototype Function of Admin
-int loginAdminAcc();
-
+//START OF MAIN FUNCTION
 int main(){
     int menu;
 	
     do{
+	system("cls");
     	printf("|+|----------------------------------|+|\n");
     	printf("|+|     |   PHARMACY INVENTORY  |    |+|\n");
     	printf("|+|----------------------------------|+|\n");
@@ -71,23 +77,36 @@ int main(){
 	    int loginAdmin = 0;
             loginAdmin = loginAdminAcc();
 	    if (loginAdmin == 1){
-	        //Login success and go to user menu
+	        //Login success and go to admin menu
+		adminMode();
 	    }
 	    else{
                 //Login failed and go to login again
 		goto loginAdmin;
 	    }
 	else if(menu == 2){
-	    loginUser:
-	    int loginUser = 0;
-            loginUser = loginUserAcc();
-	    if (loginUser == 1){
-	        //Login success and go to user menu
+	    char acc;
+	    Acc:
+	    printf("|+| Apakah anda sudah memiliki akun? (Y/N) : ");
+	    scanf("%c", &acc);
+	    if(acc == 'Y' && acc == 'y'){ //if user already have account
+	        loginUser:
+	        int loginUser = 0;
+                loginUser = loginUserAcc();
+	        if (loginUser == 1){
+	            //Login success and go to user menu
+		    userMode():
+	        }
+	        else{
+                    //Login failed and go to login again
+		    goto loginUser;
+	        }
 	    }
-	    else{
-                //Login failed and go to login again
-		goto loginUser;
+	    else{ //if user don't have account
+                createUserAcc();
+		goto acc;
 	    }
+	}
 	else if(menu == 3){
 	    exit(0);
 	}
@@ -99,7 +118,139 @@ int main(){
     
     return 0;
 }
+//END MAIN FUNCTION
 
+//START OF ADMIN'S FUNCTION 
+int adminMode(){
+    system("cls");
+    int menu;
+    while (1){
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|       PHARMACY INVENTORY      |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|--------------USER-------------|*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        1. MENU 1              |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        2. MENU 2              |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        3. MENU 3              |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        4. MENU 4              |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        5. MENU 5	          |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        6. EXIT                |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*| Pilih Menu :");
+        scanf("%d", &pilih);
+
+        switch (pilih) {
+            case 1:
+                //menu 1
+                break;
+            case 2:
+                //menu 2
+                break;
+            case 3:
+                //menu 3
+                break;
+            case 4:
+                //menu 4
+                break;
+            case 5:
+                //menu 5
+                break;
+            case 6:
+                exit(0);
+            default:
+                printf("|*| Input tidak ada dalam menu!\n");
+        }
+    }
+    return 0;
+}
+
+int loginAdminAcc(){
+    system("cls");
+    char ID[255];
+    char password[255];
+    char *token;
+    
+    printf("|*|---------------------------------|*|\n");
+    printf("|*|        PHARMACY INVENTORY       |*|\n");
+    printf("|*|---------------------------------|*|\n");
+    printf("|*|--------------ADMIN--------------|*|\n");
+    printf("|*|---------------------------------|*|\n");
+    printf("|*|            LOGIN AKUN           |*|\n");
+    printf("|*|---------------------------------|*|\n");
+	
+    printf("|*| Masukkan ID : ");
+    fgets(ID, sizeof(ID), stdin);
+    fflush(stdin);
+    printf("|*| Masukkan password : ");
+    fgets(password, sizeof(password), stdin);
+    
+    if(ID == "admin" && password == "admin"){
+        printf("|*| Login Sukses!\n");
+        return 1;
+    }
+    else{
+    	printf("|+| Username atau Password salah!\n");
+	return 0;
+    }
+}
+//END OF ADMIN'S FUNCTION
+
+//START OF USER'S FUNCTION
+int userMode(){
+    system("cls");
+    int menu;
+    while (1){
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|       PHARMACY INVENTORY      |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|--------------USER-------------|*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        1. MENU 1              |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        2. MENU 2              |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        3. MENU 3              |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        4. MENU 4              |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        5. MENU 5	          |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*|        6. EXIT                |*|\n");
+        printf("|*|-------------------------------|*|\n");
+        printf("|*| Pilih Menu :");
+        scanf("%d", &pilih);
+
+        switch (pilih) {
+            case 1:
+                //menu 1
+                break;
+            case 2:
+                //menu 2
+                break;
+            case 3:
+                //menu 3
+                break;
+            case 4:
+                //menu 4
+                break;
+            case 5:
+                //menu 5
+                break;
+            case 6:
+                exit(0);
+            default:
+                printf("|*| Input tidak ada dalam menu!\n");
+        }
+    }
+    return 0;
+}
+	
 int createUserAcc(){
     system("cls");
     int ID, yakin;
@@ -115,6 +266,7 @@ int createUserAcc(){
 	
     srand(time(0));
     ID = (10*200000)+(rand()%100000);
+    printf("|*| Create User ID Sukses!\n");
     printf("|*| ID: %d (harap ingat ID ini baik-baik!)\n", ID);
     printf("|*| Masukan password : ");
     fflush(stdin);
@@ -183,33 +335,4 @@ int loginUserAcc(){
         return 0;
     }
 }
-
-int loginAdminAcc(){
-    system("cls");
-    char ID[255];
-    char password[255];
-    char *token;
-    
-    printf("|*|---------------------------------|*|\n");
-    printf("|*|        PHARMACY INVENTORY       |*|\n");
-    printf("|*|---------------------------------|*|\n");
-    printf("|*|--------------ADMIN--------------|*|\n");
-    printf("|*|---------------------------------|*|\n");
-    printf("|*|            LOGIN AKUN           |*|\n");
-    printf("|*|---------------------------------|*|\n");
-	
-    printf("|*| Masukkan ID : ");
-    fgets(ID, sizeof(ID), stdin);
-    fflush(stdin);
-    printf("|*| Masukkan password : ");
-    fgets(password, sizeof(password), stdin);
-    
-    if(ID == "admin" && password == "admin"){
-        printf("|*| Login Sukses!\n");
-        return 1;
-    }
-    else{
-    	printf("|+| Username atau Password salah!\n");
-	return 0;
-    }
-}
+//END OF USER'S FUNCTION
