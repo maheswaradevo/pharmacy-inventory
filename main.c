2c2ccc1f49bb8a43be616c6e99be0601f96fca5a
Arthya Andika <74721722+mistykz@users.noreply.github.com>
2021-12-14 23:39:58 +0800

    fix create user's account or not in menu 2

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main()
         //Admin
         if (menu == 1)
         {
-        loginAdmin:
+            loginAdmin:
             loginAdmin = loginAdminAcc();
             if (loginAdmin == 1)
             {
@@ -46,12 +46,12 @@ int main()
         }
         else if (menu == 2)
         {
-        Acc:
+            Acc:
             printf("|+| Apakah anda sudah memiliki akun? (Y/N) : ");
             scanf("%c", &acc);
-            if (acc == 'Y' && acc == 'y')
+            if (acc == 'Y' || acc == 'y')
             { //if user already have account
-            loginUser:
+                loginUser:
                 loginUser = loginUserAcc();
                 if (loginUser == 1)
                 {
