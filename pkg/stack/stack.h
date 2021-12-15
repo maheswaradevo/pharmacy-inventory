d25fcd65d78c3efed93398c0cb3e5197cafcf4a4
Maheswara Devo <pundadevo21@gmail.com>
2021-12-15 14:35:35 +0800

    fix stack header file

--- pkg/stack/stack.h
+++ pkg/stack/stack.h
@@ -1,6 +1,16 @@
 #ifndef STACK_DOT_H
 #define STACK_DOT_H
+
 #include <stdio.h>
 #include <string.h>
+#include "stack.c"
 #define MAX 50
+
+void initiate();
+int isEmpty();
+int isFull();
+void push(char itemName[255], int date, int month, int year);
+void pop();
+void display();
+
 #endif
