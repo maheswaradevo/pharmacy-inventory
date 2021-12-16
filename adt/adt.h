15ac1d7df13888ec5cc847d0d882d42a47f4d1cc
Arthya Andika <74721722+mistykz@users.noreply.github.com>
2021-12-16 22:13:06 +0800

    delete useless struct

--- adt/adt.h
+++ adt/adt.h
@@ -4,17 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 50
-struct Person
-{
-    char username[255];
-    char password[255];
-    int token;
-};
-struct Person p;
-struct Person perArray[20];
-struct Person *hashArray[SIZE];
-struct Person *user;
-struct Person *dummy;
 
 struct dateExp
 {
